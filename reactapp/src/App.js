3d9a99be943f9fcb13f7932c487a1fc98799a058
NavaneethKB <justmailme2209@gmail.com>
2023-06-28 05:56:20 +0000

    changes-1

---
+++ reactapp/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    
+    <div className="App">
+      <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+        <span>
+          Edit <code>src/App.js</code> nd save too reloads.
+        </span>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
